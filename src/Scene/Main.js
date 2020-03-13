44259a5f598677293b8fe06afa508798966d0bd8
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-03-13 07:22:42 +0100

    commit

---
+++ src/Scene/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Main extends React.Component {
+    
+  
+
+    render(){
+        return (
+            <h1>Hello I'm Main Page</h1>
+        )
+        
+    }
+}
+
+export default Main;
