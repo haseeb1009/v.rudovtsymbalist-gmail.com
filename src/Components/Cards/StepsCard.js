b08fe8f590ee4d0068c41e4ef95d56574118a7e8
Muhammad Haseeb <muhammad.haseeb@aeratechnology.com>
2020-03-26 18:07:08 +0500

    adds animation to the stepCard

--- src/Components/Cards/StepsCard.js
+++ src/Components/Cards/StepsCard.js
@@ -1,21 +1,30 @@
-import React from 'react'
-const StepsCard = (props) => {
-    return (
-       
-        <div id={props.id} style={{ width: '60vw' }} className='stepsCard'>
-            <div id='row-tag'>
-                <div className='number' style={{ background: props.numberbckg }}>
-                    {props.number}
-                </div>
-                <h2 style={{ width: props.h2width, color: props.h2color, textAlign: 'start' }} >{props.h2}</h2>
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import '../../styles/StepsCard.scss';
+
+const StepsCard = props => {
+  return (
+    <div id={props.id} style={{ width: '60vw' }} className="stepsCard">
+      <TransitionGroup>
+        <div id="row-tag">
+          <CSSTransition key={props.number} timeout={1000} classNames="stepscardAnimation">
+            <div className="number" style={{ background: props.numberbckg }}>
+              {props.number}
             </div>
-
-            <p style={{ width: '50vw' }} >{props.p} </p>
-
+          </CSSTransition>
+          <CSSTransition key={props.h2} timeout={1000} classNames="stepscardAnimation">
+            <h2 style={{ width: props.h2width, color: props.h2color, textAlign: 'start' }}>
+              {props.h2}
+            </h2>
+          </CSSTransition>
         </div>
-     
-        
-    )
-}
+        <CSSTransition key={props.p} timeout={1000} classNames="stepscardAnimation">
+          <p style={{ width: '50vw' }}>{props.p} </p>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
 
 export default StepsCard;
