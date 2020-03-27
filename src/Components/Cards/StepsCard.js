d1cd9e0609d463c0799cc301e2a9ec9a5d4d756c
Muhammad Haseeb <muhammad.haseeb@aeratechnology.com>
2020-03-27 22:56:55 +0500

    Make animation smoother

--- src/Components/Cards/StepsCard.js
+++ src/Components/Cards/StepsCard.js
@@ -6,20 +6,20 @@ import '../../styles/StepsCard.scss';
 const StepsCard = props => {
   return (
     <div id={props.id} style={{ width: '60vw' }} className="stepsCard">
-      <TransitionGroup>
-        <div id="row-tag">
-          <CSSTransition key={props.number} timeout={1000} classNames="stepscardAnimation">
-            <div className="number" style={{ background: props.numberbckg }}>
-              {props.number}
-            </div>
-          </CSSTransition>
-          <CSSTransition key={props.h2} timeout={1000} classNames="stepscardAnimation">
+      <div id="row-tag">
+        <div className="number" style={{ background: props.numberbckg }}>
+          {props.number}
+        </div>
+        <TransitionGroup>
+          <CSSTransition key={props.h2} timeout={500} classNames="stepscardAnimation">
             <h2 style={{ width: props.h2width, color: props.h2color, textAlign: 'start' }}>
               {props.h2}
             </h2>
           </CSSTransition>
-        </div>
-        <CSSTransition key={props.p} timeout={1000} classNames="stepscardAnimation">
+        </TransitionGroup>
+      </div>
+      <TransitionGroup>
+        <CSSTransition key={props.p} timeout={500} classNames="stepscardAnimation">
           <p style={{ width: '50vw' }}>{props.p} </p>
         </CSSTransition>
       </TransitionGroup>
