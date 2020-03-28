a19c5ac49b263bc2ae9b599d4deb61059c2b40f4
Muhammad Haseeb <muhammad.haseeb@aeratechnology.com>
2020-03-28 16:21:33 +0500

    Ftech teamMembers from Firebase and adds it to TeamCards

--- src/Components/Cards/TeamCard.js
+++ src/Components/Cards/TeamCard.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import '../../styles/TeamCard.scss'
+import React from 'react';
+import '../../styles/TeamCard.scss';
 
-const TeamCard = (props) =>{
-    return(
-        <div id='teamCard'>
-          <img src={props.src} width='100px' height='100px' style={{borderRadius:'50%', display: 'block',margin: '1vw auto 0 auto'}} />
-          <div>
-            <h3>{props.name}</h3>
-          <h4 style={{font: '400 1.4vw Roboto'}}>{props.position}</h4>
-          <p style={{font: '400 1vw Roboto'}}>{props.text}</p>
-            </div>
-        </div>
-    )
-}
+const TeamCard = props => {
+  return (
+    <div id="teamCard">
+      <img
+        src={props.src}
+        width="100px"
+        height="100px"
+        style={{ borderRadius: '50%', display: 'block', margin: '1vw auto 0 auto' }}
+        alt={props}
+      />
+      <div>
+        <h3>{props.name}</h3>
+        <h4 style={{ font: '400 1.4vw Roboto' }}>{props.position}</h4>
+        <p style={{ font: '400 1vw Roboto' }}>{props.text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default TeamCard;
--- src/Scene/Team.js
+++ src/Scene/Team.js
@@ -1,53 +1,53 @@
-import React from 'react'
-import '../styles/Business.scss'
-import TeamCard from '../Components/Cards/TeamCard'
-import firebase from '../FireBase/Firebase.js'
+import React from 'react';
+import '../styles/Business.scss';
+import TeamCard from '../Components/Cards/TeamCard';
+import firebase from '../FireBase/Firebase.js';
 
-let db = firebase.firestore()
+let db = firebase.firestore();
 class Team extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    }
+      teamMembers: []
+    };
   }
 
-  getItems = () => {
-    db.collection('teamMember').get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        // doc.data() is never undefined for query doc snapshots
-        this.setState({
-          items: doc.data()
-        })
+  componentDidMount = () => {
+    this.getTeamMembers();
+  };
+
+  getTeamMembers = () => {
+    db.collection('teamMember')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          this.setState({
+            teamMembers: [...this.state.teamMembers, { ...data }]
+          });
+        });
       });
-    }) 
-  }
-  render() {
+  };
 
-    console.log(this.state.items)
+  render() {
+    console.log(this.state.teamMembers);
+    const { teamMembers } = this.state;
     return (
       <main>
-        <h1 style={{
-          font: '600 4.43vw Roboto',
-          width: '44.52vw',
-          textAlign: 'center',
-          margin: '5% auto 0 auto'
-        }}>EXECUTIVE TEAM</h1>
-        <p style={{
-          width: '57.35vw',
-          font: '400 1.82vw Roboto',
-          margin: '5% auto 2% auto',
-          textAlign: 'center'
-        }}>Admin office</p>
-
-
+        {teamMembers.map(member => {
+          return (
+            <TeamCard
+              name={member.name}
+              position={member.position}
+              src={member.url}
+              text={member.text}
+            />
+          );
+        })}
 
         <button onClick={this.getItems}>Get</button>
-
-
       </main>
-    )
-
+    );
   }
 }
 
